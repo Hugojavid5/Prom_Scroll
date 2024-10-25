6c484720bb5facd502d2ba81420079bf3c472315
hugojavid5 <hugojavidferrero@gmail.com>
2024-10-25 20:22:38 +0200

    Clase TaskAdapter creado

---
+++ app/src/main/java/com/hugo/scroll_infinito/ui/theme/TaskAdapter.kt
@@ -0,0 +1,20 @@
+package com.hugo.scroll_infinito.ui.theme
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.hugo.scroll_infinito.TaskViewHolder
+
+class TaskAdapter(private val tasks: List<String>, private val onItemDone: (Int) -> Unit) : RecyclerView.Adapter<TaskViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        // Infla el diseño del item de tarea
+        return TaskViewHolder(layoutInflater.inflate(R.layout.item_task, parent, false))
+    }
+    override fun getItemCount() = tasks.size
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        holder.render(tasks[position], onItemDone) // Renderiza la tarea en el ViewHolder
+    }
+}
