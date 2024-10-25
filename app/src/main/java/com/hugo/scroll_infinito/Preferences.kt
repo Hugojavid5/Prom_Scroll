bf9bf535cff2826b239e4b45210885c037dacc15
hugojavid5 <hugojavidferrero@gmail.com>
2024-10-25 20:41:25 +0200

    Clase Preferences con javadocs

--- app/src/main/java/com/hugo/scroll_infinito/Preferences.kt
+++ app/src/main/java/com/hugo/scroll_infinito/Preferences.kt
@@ -3,7 +3,11 @@ package com.hugo.scroll_infinito
 import android.content.Context
 import android.content.SharedPreferences
 
-
+/**
+ * Clase que maneja el almacenamiento de preferencias utilizando SharedPreferences.
+ *
+ * @param context Contexto de la aplicación para acceder a SharedPreferences.
+ */
 class Preferences(context: Context) {
 
     companion object {
@@ -14,10 +18,21 @@ class Preferences(context: Context) {
     // Inicializa SharedPreferences
     val prefs: SharedPreferences = context.getSharedPreferences(PREFS_NAME, 0)
 
+    /**
+     * Guarda una lista de tareas en SharedPreferences.
+     *
+     * @param tasks Lista de tareas a almacenar.
+     */
     fun saveTasks(tasks: List<String>) {
         // Convierte la lista de tareas en un conjunto y lo guarda
         prefs.edit().putStringSet(TASKS, tasks.toSet()).apply()
     }
+
+    /**
+     * Recupera la lista de tareas almacenadas en SharedPreferences.
+     *
+     * @return Lista mutable de tareas.
+     */
     fun getTasks(): MutableList<String> {
         // Obtiene el conjunto de tareas y lo convierte en una lista mutable
         return prefs.getStringSet(TASKS, emptySet<String>())?.toMutableList() ?: mutableListOf()
