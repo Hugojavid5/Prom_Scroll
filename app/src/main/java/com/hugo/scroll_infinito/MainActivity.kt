52c843414387fa7d408f78a16be628d624f1e9b4
hugojavid5 <hugojavidferrero@gmail.com>
2024-10-25 20:33:50 +0200

    Añadido el sonido al añadir una tarea

--- app/src/main/java/com/hugo/scroll_infinito/MainActivity.kt
+++ app/src/main/java/com/hugo/scroll_infinito/MainActivity.kt
@@ -6,11 +6,8 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.hugo.scroll_infinito.TaskApplication.Companion.prefs
 
-/**
- * MainActivity es la actividad principal de la aplicación que permite al usuario gestionar una lista de tareas.
- * Proporciona funcionalidades para agregar, eliminar y mostrar tareas, así como reproducir un sonido al eliminar.
- */
 class MainActivity : AppCompatActivity() {
 
     lateinit var btnAddTask: Button // Botón para añadir una nueva tarea
@@ -22,12 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     var tasks = mutableListOf<String>() // Lista mutable que contiene las tareas
 
-    /**
-     * Método de creación de la actividad.
-     * Inicializa la interfaz de usuario y el MediaPlayer.
-     *
-     * @param savedInstanceState Estado guardado de la actividad, si existe.
-     */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -36,54 +28,36 @@ class MainActivity : AppCompatActivity() {
         // Inicializar MediaPlayer con el archivo de sonido
         //mediaPlayer = MediaPlayer.create(this, R.raw.delete_sound.mp3)
     }
-
-    /**
-     * Inicializa la interfaz de usuario, incluyendo vistas, oyentes y el RecyclerView.
-     */
     private fun initUi() {
         initView()
         initListeners()
         initRecyclerView()
     }
 
-    /**
-     * Inicializa el RecyclerView con la lista de tareas.
-     * Configura el adaptador y el administrador de diseño.
-     */
+
     private fun initRecyclerView() {
         tasks = prefs.getTasks() // Obtener tareas guardadas
         rvTasks.layoutManager = LinearLayoutManager(this) // Configurar el diseño lineal
-        adapter = TaskAdapter(tasks) { deleteTask(it) } // Crear el adaptador con la lista de tareas
+        adapter = TaskAdapter(tasks) { addTask(it.toString()) } // Crear el adaptador con la lista de tareas
         rvTasks.adapter = adapter // Asignar el adaptador al RecyclerView
     }
 
-    /**
-     * Elimina una tarea de la lista en la posición especificada.
-     * Reproduce un sonido de eliminación después de eliminar la tarea.
-     *
-     * @param position Posición de la tarea a eliminar.
-     */
-    private fun deleteTask(position: Int) {
-        tasks.removeAt(position) // Eliminar la tarea de la lista
+    private fun addTask(newTask: String) {
+        tasks.add(newTask) // Agregar la nueva tarea a la lista
         adapter.notifyDataSetChanged() // Notificar al adaptador que los datos han cambiado
         prefs.saveTasks(tasks) // Guardar la lista actualizada
 
-        // Reproducir sonido de eliminación
+        // Reproducir sonido de inserción
         mediaPlayer.start()
     }
 
-    /**
-     * Inicializa los oyentes de eventos para las vistas.
-     * En particular, configura el oyente del botón para añadir tareas.
-     */
+
+
     private fun initListeners() {
         btnAddTask.setOnClickListener { addTask() }
     }
 
-    /**
-     * Agrega una nueva tarea a la lista si el campo de texto no está vacío.
-     * Actualiza el RecyclerView y guarda la lista de tareas.
-     */
+
     private fun addTask() {
         val taskToAdd: String = etTask.text.toString() // Obtener el texto de la tarea
         if (taskToAdd.isNotEmpty()) { // Verificar que el campo no esté vacío
